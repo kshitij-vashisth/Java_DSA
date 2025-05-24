edab7f00a3ac8fe31d2eeb480eb27591216ee280
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-24 12:31:37 +0100

    [1stcommit]

---
+++ src/FirstProgram.java
@@ -0,0 +1,8 @@
+public class FirstProgram {
+    public static void main(String[] args) {
+        // after typing sout press tab
+        /* multi line
+        comment */
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/Variables.java
@@ -0,0 +1,17 @@
+public class Variables {
+    public static void main(String[] args) {
+        int a; // declaration
+        a = 23; // initialization
+        String name = "Kshitij";
+        System.out.println(a); // if we use print instead of println, output will be 23Kshitij
+        System.out.println(name);
+        System.out.print("6"+"4");
+
+        int x = 23;
+        int y = 45;
+        int sum = x + y;
+        System.out.println("sum of "+x+" & "+y+" is "+sum+".");
+        System.out.println("sum of "+x+" & "+y+" is "+x+y+".");
+        System.out.println("sum of "+x+" & "+y+" is "+(x+y)+".");
+    }
+}
