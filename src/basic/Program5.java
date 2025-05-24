ccb696aabee010c115fbd7cfa77dc639f62cd65c
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-24 22:50:51 +0100

    [MathAPI] learnt the use of math api

---
+++ src/basic/Program5.java
@@ -0,0 +1,12 @@
+package basic;
+
+public class Program5 {
+    public static void main(String[] args) {
+        System.out.println(Math.sqrt(4));
+        System.out.println(Math.cbrt(27));
+        System.out.println(Math.pow(2,3));
+        System.out.println(Math.round(4.49));
+        System.out.println(Math.max(4,5));
+        System.out.println(Math.min(4,5));
+    }
+}
