c0dd4b1c4798976413b6f20280ed5725b87e0d61
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-24 15:08:31 +0100

    [Problem4] solved Problem 4

--- src/ProgramFour.java
+++ src/ProgramFour.java
@@ -1,2 +1,13 @@
+import java.util.Scanner;
 public class ProgramFour {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter name:"); // avoided using nextline because it takes new line also
+        String name = sc.next();
+        System.out.println("Enter Age:");
+        byte age = sc.nextByte();
+        System.out.println("Hello "+name+", you are "+age+" years old.");
+        //please note, if you are using nextline, then if there is any scanner before it,
+        // best practice is to use a dummy nextline before using it
+    }
 }
