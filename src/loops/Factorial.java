5899ede6bf0a09138bd5201a65bf97c1088f6441
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-25 22:51:56 +0100

    [Factorial] solved factorial problem using big integer method

--- src/loops/Factorial.java
+++ src/loops/Factorial.java
@@ -1,4 +1,5 @@
 package loops;
+import java.math.BigInteger;
 import java.util.Scanner;
 public class Factorial {
     public static void main(String[] args) {
@@ -10,5 +11,14 @@ public class Factorial {
             factorial *= n-1;
         }
         System.out.println(factorial);
+
+        // using big integer
+        System.out.println("Enter number to calculate factorial for (big integer):");
+        int o = sc.nextInt();
+        BigInteger fact = new BigInteger("1");
+        for (int i =1; i <= o; i++) {
+            fact = fact.multiply(new BigInteger(Integer.toString(i)));
+        }
+        System.out.println(fact);
     }
 }
