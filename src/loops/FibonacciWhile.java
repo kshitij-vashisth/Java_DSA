ef3135ce36e0f4e920a5a27ad2ba53c2beb4fd13
KayVeeZ <kshitijvashisth@gmail.com>
2025-05-28 14:03:46 +0100

    [FibWhile] wrote fibonacii with while

--- src/loops/FibonacciWhile.java
+++ src/loops/FibonacciWhile.java
@@ -2,13 +2,13 @@ package loops;
 
 public class FibonacciWhile {
     public static void main(String[] args) {
-        int n = 9;
+        int n = 10;
         System.out.print(0+" "+1+" ");
         int n1 = 0;
         int n2 = 1;
         int n3 = 0;
         int i = 2;
-        while (i <9){
+        while (i <n){
             n3 = n1 + n2;
             System.out.print(n3+" ");
             n1 = n2;
